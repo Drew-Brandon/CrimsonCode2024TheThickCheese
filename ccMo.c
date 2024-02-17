026e9e4dab5b59eb01c0c9c0035166b4d21445f7
mohassan9989 <160357159+mohassan9989@users.noreply.github.com>
2024-02-17 12:31:03 -0800

    Create ccMo.c
    can writr here

---
+++ ccMo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "finance_manager.h"
+
+int main() {
+    printf("Welcome to Personal Finance Manager\n");
+    // Initialization code here
+    
+    while (1) {
+        // Main loop for user input and action dispatch
+    }
+    
+    return 0;
+}
